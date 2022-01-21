0c761547c0bbae09b3f33be745b49f0820e93f89
hsmtkk <hsmtkk@gmail.com>
2022-01-21 14:28:24 +0900

    Go coding done

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/hsmtkk/turbo-funicular
 
 go 1.17
+
+require (
+	github.com/labstack/echo/v4 v4.6.3
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.28.0
+	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.3.0
+	go.opentelemetry.io/otel/sdk v1.3.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.opentelemetry.io/otel/trace v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+
+	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+)
+
+func main() {
+	exporter, err := jaeger.New(jaeger.WithAgentEndpoint())
+	if err != nil {
+		log.Fatalf("failed to get exporter; %s", err)
+	}
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceNameKey.String("turbo-funicular"),
+			semconv.ServiceVersionKey.String("1.0"))),
+	)
+	otel.SetTracerProvider(traceProvider)
+	defer func() {
+		if err := traceProvider.Shutdown(context.Background()); err != nil {
+			log.Printf("failed to shutdown trace provider; %s", err)
+		}
+	}()
+
+	e := echo.New()
+	e.Use(otelecho.Middleware("turbo-funicular"))
+	e.GET("/users/:id", handleUsers)
+	e.Start(":8000")
+}
+
+func handleUsers(ctx echo.Context) error {
+	userID := ctx.Param("id")
+	name := getUser(ctx.Request().Context(), userID)
+	result := map[string]string{"id": userID, "name": name}
+	return ctx.JSON(http.StatusOK, result)
+}
+
+func getUser(ctx context.Context, id string) string {
+	tr := otel.Tracer("turbo-funicular")
+	_, span := tr.Start(ctx, "getUser")
+	defer span.End()
+	switch id {
+	case "1":
+		return "alpha"
+	case "2":
+		return "bravo"
+	default:
+		return "unknown"
+	}
+}
